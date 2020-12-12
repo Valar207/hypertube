5faa34323ead93cb217b6f7761c429c7a3d2cba7
Valar207 <46608441+Valar207@users.noreply.github.com>
2020-12-12 23:04:50 +0100

    remove env

--- server/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (res, statusCode, param, msg) => {
-  return res.status(statusCode).json({
-    status: statusCode,
-    param: param,
-    message: msg,
-  });
-};
-
-module.exports = errorHandler;
