555b693222d750345814852b8fd2e915fd56a9a3
Valar207 <46608441+Valar207@users.noreply.github.com>
2020-12-12 23:06:15 +0100

    remove env

---
+++ server/services/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const URI = process.env.DB_URL;
+
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  promiseLibrary: global.Promise,
+  keepAlive: 1,
+  connectTimeoutMS: 30000,
+};
+
+const connectDB = async (req, res, next) => {
+  await mongoose.connect(URI, options, () => {
+    console.log("CONNECTED TO DB...");
+  });
+  next();
+};
+
+module.exports = connectDB;
