09285822176b13adceb35e76dd18270cb9647f23
GlorianB <gm.bikoumou@gmail.com>
2021-02-03 01:11:05 +0100

    comments done

---
+++ client/src/service/movie.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = "http://localhost:5000/api/v1";
+const movie = url + "/movie";
+
+export const fetchMovieDetails = async (movie_id) => {
+  try {
+    const { data } = await axios.get(movie + `/${movie_id}`, {
+      withCredentials: true,
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postMovieDetails = async (movieData) => {
+  try {
+    const { data } = await axios.post(movie, {
+      withCredentials: true,
+      body: movieData,
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
