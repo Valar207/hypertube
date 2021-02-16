5bce2806c73a14ea8557d16bc4458e3e03de074a
Valar207 <46608441+Valar207@users.noreply.github.com>
2021-02-16 12:59:04 +0100

    remove files at 3am working

--- server/src/api/services/cronjob.js
+++ server/src/api/services/cronjob.js
@@ -5,7 +5,7 @@ const removeDir = require("../utils/removeDir");
 const pathToMovies = process.cwd() + "/Movies";
 
 const job = new CronJob(
-  "3 0 * * *", // Tous les jours a 03:00
+  "0 3 * * *", // Tous les jours a 03:00
   () => {
     removeDir(pathToMovies);
     console.log("Movies have been autodeleted");
